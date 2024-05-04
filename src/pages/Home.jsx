c79eb22638d28fe44438b1ae4ecd5a79783c04b5
abrown95 <abrown95@myune.edu.au>
2024-05-04 19:14:00 +1000

    first

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import './Home.css'
+import {ReactComponent as Logo} from '../assets/img/logo.svg';
+import {ReactComponent as Icon} from '../assets/img/iconsvg.svg';
 
-function HomeScreen(props) {
+function Home() {
+
+    const [scale, setScale] = useState('50%');
+    useEffect(() => {
+        setScale('300')
+    }, [])
     return (
-        <div>
+        <div className='main'>
+            <div className="container">
 
+                    <Icon style={{
+                        height:scale,
+                        width:scale,
+                        transition: "all 2.0s",
+                        transitionDelay: '2s'
+                    }}/>
+                    <Logo style={{
+                        height:scale,
+                        width:scale,
+                        transition: "all 2.0s",
+                        transitionDelay: '2s'
+                    }}/>
+                </div>
+
+                <div className="navigation">
+                    {/*<h1>HELLO</h1>*/}
+                    {/*<p>Hello Cunt</p>*/}
+                </div>
         </div>
     );
 }
 
-export default HomeScreen;
+export default Home;
