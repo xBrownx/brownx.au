f239dc3dbe16da5d6c54473a80bea94de40b80c0
asbro <abrown95@myune.edu.au>
2024-05-05 10:36:01 +1000

    laptop push

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,32 +5,47 @@ import {ReactComponent as Icon} from '../assets/img/iconsvg.svg';
 
 function Home() {
 
-    const [scale, setScale] = useState('50%');
+    const [logoHeight, setLogoHeight] = useState('100%');
+    const [logoWidth, setLogoWidth] = useState('100%');
+
+    const [textSize, setTextSize] = useState('25em');
+    const [textVisible, setTextVisible] = useState(0);
+
     useEffect(() => {
-        setScale('300')
+        setLogoHeight('9vh')
+        setLogoWidth('16vh')
+        setTextVisible(1)
     }, [])
     return (
         <div className='main'>
             <div className="container">
-
-                    <Icon style={{
-                        height:scale,
-                        width:scale,
-                        transition: "all 2.0s",
-                        transitionDelay: '2s'
-                    }}/>
+                <div className="logo-container">
                     <Logo style={{
-                        height:scale,
-                        width:scale,
+                        position: 'absolute',
+                        height: logoHeight,
+                        width: logoWidth,
                         transition: "all 2.0s",
                         transitionDelay: '2s'
                     }}/>
                 </div>
+            </div>
 
-                <div className="navigation">
-                    {/*<h1>HELLO</h1>*/}
-                    {/*<p>Hello Cunt</p>*/}
-                </div>
+
+            <div className="navigation" style={{
+                        position: 'flex',
+                        justifyContent: 'flex-end',
+                        alignContent: 'end',
+                        width: '100%',
+                        visibility: textVisible,
+                        opacity: textVisible,
+                        transition: "all 2.0s",
+                        transitionDelay: '4s',
+                        alignSelf: 'flex-end',
+                        right: '0px',
+                        flexDirection: "row",
+                    }}>
+                <p>Hello Cunt</p>
+            </div>
         </div>
     );
 }
